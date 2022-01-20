6aa1b57142f48f44679e7bc99b2be8ae17f3544d
Danielle Turner <danielle.7.turner@gmail.com>
2022-01-19 23:52:44 -0600

    more attributes added

--- coffeeshop.js
+++ coffeeshop.js
@@ -14,12 +14,16 @@
 
 
 
-//menu pop up functioin
+//menu pop up function ice coffee
 function menu () {
     var popup = document.getElementById("iceCoffee");
     popup.classList.toggle("show");
 }
 
+function mocha() {
+    var popup = document.getElementById("myMocha");
+    popup.classList.toggle("show");
+}
 // document.getElementById("ICF3").addEventListener("click", function(){
 //     document.getElementById("goodstuff").innerHTML = "1/2 cup milk";
 // });
