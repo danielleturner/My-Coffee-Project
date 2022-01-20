72667c85cb6e6fe64fa93893c0bc2b197fd0caa1
Danielle Turner <danielle.7.turner@gmail.com>
2022-01-19 21:59:11 -0600

    My Coffee Project

---
+++ coffeeshop.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//menu pop up functioin
+function menu () {
+    var popup = document.getElementById("iceCoffee");
+    popup.classList.toggle("show");
+}
+
+// document.getElementById("ICF3").addEventListener("click", function(){
+//     document.getElementById("goodstuff").innerHTML = "1/2 cup milk";
+// });
