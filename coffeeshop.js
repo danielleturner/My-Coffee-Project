ed359df485fb2f7c46b7bec2c81a49d8a7af06e7
Danielle Turner <danielle.7.turner@gmail.com>
2022-01-24 09:11:22 -0600

    ingredient added

--- coffeeshop.js
+++ coffeeshop.js
@@ -14,7 +14,7 @@
 
 
 
-//menu pop up function ice coffee
+//menu pop up function ice coffee cnt1
 function menu () {
     var popup = document.getElementById("iceCoffee");
     popup.classList.toggle("show");
@@ -29,6 +29,34 @@ function good() {
     var popup = document.getElementById("brown");
     popup.classList.toggle("show");
 }
+
+function coldLatte() {
+    var popup = document.getElementById("cold");
+    popup.classList.toggle("show")
+}
+
+//menu pop up function ice coffee cnt2
+
+function coffee() {
+    var popup = document.getElementById("myCoffee");
+    popup.classList.toggle("show")
+}
+
+function hotMocha() {
+    var popup = document.getElementById("hot");
+    popup.classList.toggle("show")
+}
+
+function caramel() {
+    var popup = document.getElementById("myCaramel");
+    popup.classList.toggle("show")
+}
+
+function seasonal() {
+    var popup = document.getElementById("valentine");
+    popup.classList.toggle("show")
+}
+
 
 // document.getElementById("ICF3").addEventListener("click", function(){
 //     document.getElementById("goodstuff").innerHTML = "1/2 cup milk";
